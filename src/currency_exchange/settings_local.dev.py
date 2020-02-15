1e69ec86a536a1754978c9d8c8111b14f49164a1
Vadym Nedoinov <vadim.nedoynov@gmail.com>
2020-02-15 11:26:32 +0200

    Initial commit

---
+++ src/currency_exchange/settings_local.dev.py
@@ -0,0 +1,7 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'h=i^_robf7s+hjzn_(*-scw(dw*7sanr(@&+^o)2k39511b%6s'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ['*']
