3e55c26a21085bfeb96841f76c530dd7ec9b3695
Vadym Nedoinov <vadim.nedoynov@gmail.com>
2020-02-17 21:46:13 +0200

    yml file and dockerfile added, runserver in docker done

--- src/currency_exchange/settings.py
+++ src/currency_exchange/settings.py
@@ -129,7 +129,7 @@ EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
 CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
-        'LOCATION': '127.0.0.1:11211',
+        'LOCATION': 'memcached',
     }
 }
 
